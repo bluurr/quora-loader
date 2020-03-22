def103d87e4bc76f6607c87bbc21c7f5db35a464
Chris Bath <5076113+bluurr@users.noreply.github.com>
2020-03-22 12:52:29 +0000

    Extracted mvn upload script nd added conditional check for deployment.

---
+++ cicd/script/mvn-upload.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+POM_FILE=$(ls *.pom)
+
+echo "Using POM_FILE: ${POM_FILE}"
+
+## extract values required
+VERSION=$(mvn -f ${POM_FILE} help:evaluate -Dexpression=project.version -q -DforceStdout)
+GROUP_ID=$(mvn -f ${POM_FILE} help:evaluate -Dexpression=project.groupId -q -DforceStdout)
+ARTIFACT_ID=$(mvn -f ${POM_FILE} help:evaluate -Dexpression=project.artifactId -q -DforceStdout)
+
+ARTIFACT_ID_WITH_VERSION="${ARTIFACT_ID}-${VERSION}"
+
+echo "MVN: ${GROUP_ID}:${ARTIFACT_ID}:${VERSION}"
+
+## Upload files to nexus
+mvn deploy:deploy-file \
+    -DgroupId=${GROUP_ID} \
+    -DartifactId=${ARTIFACT_ID} \
+    -Dversion=${VERSION} \
+    -Dpackaging=jar \
+    -DpomFile=${ARTIFACT_ID_WITH_VERSION}.pom \
+    -Dfile=${ARTIFACT_ID_WITH_VERSION}.jar \
+    -Dfiles="${ARTIFACT_ID_WITH_VERSION}-javadoc.jar.asc,${ARTIFACT_ID_WITH_VERSION}-sources.jar.asc,${ARTIFACT_ID_WITH_VERSION}.jar.asc,${ARTIFACT_ID_WITH_VERSION}.pom.asc" \
+    -Dclassifiers="javadoc,sources,jar,pom" \
+    -Dtypes="asc,asc,asc,asc" \
+    -Dsources=${ARTIFACT_ID_WITH_VERSION}-sources.jar \
+    -Djavadoc=${ARTIFACT_ID_WITH_VERSION}-javadoc.jar \
+    -DrepositoryId=sonatype-nexus-staging-bluurr \
+    -Durl=${URL}
