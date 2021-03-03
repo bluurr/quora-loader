e33c7bf2820280cc04bcbbd7c86d8be9f9dc8f95
Bluurr <5076113+bluurr@users.noreply.github.com>
2021-03-03 13:57:41 +0000

    Added configuration to tune headless mode.

--- quora-api/src/main/kotlin/com/bluurr/quora/config/WebDriverConfiguration.kt
+++ quora-api/src/main/kotlin/com/bluurr/quora/config/WebDriverConfiguration.kt
@@ -2,11 +2,12 @@ package com.bluurr.quora.config
 
 import com.bluurr.quora.client.BaseQuoraClient
 import com.bluurr.quora.client.QuoraClient
-import com.bluurr.quora.model.user.LoginCredential
 import com.bluurr.quora.extension.EnhancedDriver
+import com.bluurr.quora.model.user.LoginCredential
 import io.github.bonigarcia.wdm.WebDriverManager
 import io.github.bonigarcia.wdm.config.DriverManagerType.CHROME
 import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeOptions
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.context.annotation.Bean
@@ -31,7 +32,7 @@ class WebDriverConfiguration {
     @PostConstruct
     fun setupDriver() {
 
-        WebDriverManager.getInstance(CHROME).setup();
+        WebDriverManager.getInstance(CHROME).setup()
     }
 
     @Component
@@ -39,11 +40,28 @@ class WebDriverConfiguration {
 
         fun createDriver(): EnhancedDriver {
 
-            return EnhancedDriver(driverProperties.baseUrl, ChromeDriver())
+            return EnhancedDriver(driverProperties.baseUrl, chromeDriver())
+        }
+
+        private fun chromeDriver() : ChromeDriver {
+
+            val options = chromeOptions()
+
+            return ChromeDriver(options)
+        }
+
+        private fun chromeOptions() : ChromeOptions {
+
+            val options = ChromeOptions()
+
+            if (driverProperties.headless) {
+                options.addArguments("--headless")
+            }
+            return options
         }
     }
 }
 
 @ConstructorBinding
 @ConfigurationProperties("web.driver")
-data class WebDriverProperties(val baseUrl: String)
+data class WebDriverProperties(val baseUrl: String, val headless: Boolean)
